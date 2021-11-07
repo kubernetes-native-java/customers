3fa76391e043f7c0d76c918d40442ca747f82034
Josh Long <josh@joshlong.com>
2021-11-07 13:35:01 -0800

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 cd $GITHUB_WORKSPACE
-echo "the root is $ROOT"
-mvn -DskipTests clean package spring-boot:build-image
 
+PREFIX=knj
+IMAGE=customers
+IMAGE_NAME=$PREFIX/$IMAGE
 
+./mvnw -DskipTests=true clean package spring-boot:build-image -Dspring-boot.build-image.imageName=$IMAGE_NAME
+docker run  -p 8081:8081 -e SERVER_PORT=8081 $IMAGE_NAME
