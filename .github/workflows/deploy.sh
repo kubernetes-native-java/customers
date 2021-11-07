7b58ecfb73e948000e549c155bfd4da4816695ab
Josh Long <josh@joshlong.com>
2021-11-07 13:54:55 -0800

    up

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
-cd $GITHUB_WORKSPACE
+cd "${GITHUB_WORKSPACE:-$(dirname $0)/../..}"
+pwd
+MANIFESTS_DIR=$(pwd)/k8s/manifests
 PREFIX=bootiful
 APP_NAME=customers
-IMAGE=$APP_NAME
-IMAGE_NAME=$PREFIX/$IMAGE
-
-./mvnw -DskipTests=true clean package spring-boot:build-image -Dspring-boot.build-image.imageName=$IMAGE_NAME
-#docker run  -p 8081:8081 -e SERVER_PORT=8081 $IMAGE_NAME
-IMAGE_ID=$(docker images -q $APP_NAME)
-docker tag "${IMAGE_ID}" $IMAGE_NAME
+NS=knj
+APP_NAME=customers
+IMAGE_NAME=gcr.io/bootiful/customers:latest
+./mvnw -DskipTests=true clean package spring-boot:build-image -Dspring-boot.build-image.imageName=${IMAGE_NAME}
+# docker run -e SPRING_PROFILES_ACTIVE=cloud -e SERVER_PORT=8082 -p 8082:8082 $IMAGE_NAME
 docker push $IMAGE_NAME
+echo "pushed the image to ${IMAGE_NAME}"
+mkdir -p $MANIFESTS_DIR
+kubectl create ns $NS  -o yaml > $MANIFESTS_DIR/namespace.yaml
+kubectl -n $NS create deployment  --image=$IMAGE_NAME $APP_NAME -o yaml > $MANIFESTS_DIR/deployment.yaml
+kubectl -n $NS expose deployment $APP_NAME --port=8080 -o yaml > $MANIFESTS_DIR/service.yaml
+sleep 5
+kubectl -n $NS port-forward deployment/$APP_NAME 8080:8080  &
+
+curl -s localhost:8080/actuator/health/liveness | jq
+
+
