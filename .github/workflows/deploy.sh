003ed60e28c4f2a7a0de06c07344fcf131cf18f0
Josh Long <josh@joshlong.com>
2021-11-07 13:42:37 -0800

    deploy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -6,4 +6,7 @@ IMAGE=customers
 IMAGE_NAME=$PREFIX/$IMAGE
 
 ./mvnw -DskipTests=true clean package spring-boot:build-image -Dspring-boot.build-image.imageName=$IMAGE_NAME
-docker run  -p 8081:8081 -e SERVER_PORT=8081 $IMAGE_NAME
+#docker run  -p 8081:8081 -e SERVER_PORT=8081 $IMAGE_NAME
+IMAGE_ID=$(docker images -q $APP_NAME)
+docker tag "${IMAGE_ID}" $IMAGE_NAME
+docker push $IMAGE_NAME
