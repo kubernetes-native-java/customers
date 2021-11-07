cc9e32f177fd5a767bbf2a97eda93f8e49e2e3a1
Josh Long <josh@joshlong.com>
2021-11-07 12:43:10 -0800

    git

---
+++ src/main/java/com/example/customers/CustomersApplication.java
@@ -0,0 +1,66 @@
+package com.example.customers;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.availability.AvailabilityChangeEvent;
+import org.springframework.boot.availability.LivenessState;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class CustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersApplication.class, args);
+	}
+}
+
+@Log4j2
+@ResponseBody
+@Controller
+@RequiredArgsConstructor
+class HealthController {
+
+	private final ApplicationContext applicationContext;
+
+	@PostMapping("/down")
+	void down() {
+		AvailabilityChangeEvent.publish(this.applicationContext, LivenessState.BROKEN);
+		log.info("setting " + LivenessState.class.getName() + " to " + LivenessState.BROKEN);
+	}
+}
+
+
+@ResponseBody
+@Controller
+@RequiredArgsConstructor
+class CustomerRestController {
+
+	private final CustomerRepository repository;
+
+	@GetMapping("/customers")
+	Flux<Customer> get() {
+		return this.repository.findAll();
+	}
+}
+
+interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Customer {
+	private Integer id;
+	private String name;
+}
