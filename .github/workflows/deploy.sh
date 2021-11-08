31c0a7cca1f04f1b9d13861d1aa96b9a5317dc92
Josh Long <josh@joshlong.com>
2021-11-07 19:33:00 -0800

    templatized

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-cd "${GITHUB_WORKSPACE:-$(dirname $0)/../..}"
+ROOT=${GITHUB_WORKSPACE:-$( cd $(dirname  $0)/../.. && pwd )}/.github/workflows
+
+
 
 function module_name(){
   echo "gcr.io/${GCLOUD_PROJECT}/$1"
@@ -11,9 +13,19 @@ function deploy_module(){
   NAME=$1
   IMAGE_NAME=$(module_name "$NAME")
   docker rmi -f $IMAGE_NAME
-  ./mvnw -DskipTests=true clean package spring-boot:build-image \
-        -Dspring-boot.build-image.imageName=$IMAGE_NAME
+  ./mvnw -DskipTests=true clean package spring-boot:build-image -Dspring-boot.build-image.imageName=$IMAGE_NAME
   docker push $IMAGE_NAME
+  OUTPUT_FN=tmp/${NAME}-generated.yaml
+  mkdir -p "$(dirname $OUTPUT_FN)"
+  cat k8s.yaml.template |
+    sed -e 's,<NS>,'${K8S_NS}',g' | \
+    sed -e 's,<APP>,'${NAME}',g'  | \
+    sed -e 's,<GCR_PROJECT>,'${GCLOUD_PROJECT}',g' > $OUTPUT_FN
+  cat $OUTPUT_FN
+  kubectl apply -f $OUTPUT_FN -n $K8S_NS
 }
 
+echo "starting in $ROOT. "
+cd $ROOT
 deploy_module customers
+
