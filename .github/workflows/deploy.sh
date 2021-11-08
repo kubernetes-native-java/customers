8795a637e1357c9ccd63c0c2990fc01abce79f68
Josh Long <josh@joshlong.com>
2021-11-07 23:15:13 -0800

    up

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -3,31 +3,31 @@ set -e
 
 ROOT=${GITHUB_WORKSPACE:-$(cd $(dirname $0)/../.. && pwd)}
 
-function module_name() {
+function image_name() {
   echo "gcr.io/${GCLOUD_PROJECT}/$1"
 }
 
-function deploy_module() {
+function app_push() {
   NAME=$1
-  IMAGE_NAME=$(module_name "$NAME")
+  IMAGE_NAME=$(image_name "$NAME")
   OUTPUT_FN=$ROOT/tmp/${NAME}-generated.yaml
 
   mkdir -p "$(dirname $OUTPUT_FN)" || echo "could not create ${OUTPUT_FN}."
 
-  docker rmi -f $IMAGE_NAME || echo "no local image to delete..."
-  cd $ROOT && ./mvnw -DskipTests=true clean package spring-boot:build-image -Dspring-boot.build-image.imageName=$IMAGE_NAME
-  docker push $IMAGE_NAME
+#  docker rmi -f $IMAGE_NAME || echo "no local image to delete..."
+#  cd $ROOT && ./mvnw -DskipTests=true clean package spring-boot:build-image -Dspring-boot.build-image.imageName=$IMAGE_NAME
+#  docker push $IMAGE_NAME
 
   cat $ROOT/.github/workflows/k8s.yaml.template |
     sed -e 's,<NS>,'${K8S_NS}',g' |
     sed -e 's,<APP>,'${NAME}',g' |
     sed -e 's,<GCR_PROJECT>,'${GCLOUD_PROJECT}',g' >$OUTPUT_FN
-  cat $OUTPUT_FN
+  cat "$OUTPUT_FN"
   kubectl delete ns/$K8S_NS || echo "couldn't delete the $K8S_NS namespace. Maybe it doesn't exist?"
-  kubectl apply -f $OUTPUT_FN -n $K8S_NS || echo "could not deploy."
+  kubectl apply -f "$OUTPUT_FN" -n "$K8S_NS" || echo "could not deploy."
 }
 
 echo "starting in $ROOT. "
 cd $ROOT/../..
 ls -la
-deploy_module customers
+app_push customers
