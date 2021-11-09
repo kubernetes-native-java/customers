b101441e6e1b4101b2864c725720659d32ae802f
Josh Long <josh@joshlong.com>
2021-11-08 22:59:58 -0800

    Update deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -5,9 +5,12 @@ echo "trying to deploy ${APP_NAME}..."
 
 ROOT=${GITHUB_WORKSPACE:-$(cd $(dirname $0)/../.. && pwd)}
 IMAGE_NAME="gcr.io/${GCLOUD_PROJECT}/${APP_NAME}"
+cd $ROOT 
 
 docker rmi -f $IMAGE_NAME || echo "no local image to delete..."
-cd $ROOT && ./mvnw -DskipTests=true clean package spring-boot:build-image -Dspring-boot.build-image.imageName=$IMAGE_NAME
+
+./mvnw -DskipTests=true clean package spring-boot:build-image -Dspring-boot.build-image.imageName=$IMAGE_NAME
+
 docker push $IMAGE_NAME
 
 curl -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token ${GH_PAT}" \
